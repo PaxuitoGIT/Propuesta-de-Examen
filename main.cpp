6fcb3266ce6373ec1ffad37d2b75d13ca59f2abe
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-11-07 10:17:49 +0000

    Verificacion del teorema

--- main.cpp
+++ main.cpp
@@ -5,8 +5,18 @@ int main() {
     int a, b, c;
 
     for (a = 1; a <= 20; a++) {
-        for (b = a; b<= 20; b++) {
+        for (b = a; b <= 20; b++) {
             double c_double = sqrt (a*a + b*b);
+            c = c_double;
+
+            
+            if (c_double == c) {
+                if (a + b > c && a + c > b && b + c > a) {
+                    printf ("%d - %d -%d\n", a,b,c);
+                }
+            }
         }
     }
+
+    return 0;
 }
