59bc91590f495d2760c94061534a22f87480fab9
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-11-07 10:11:58 +0000

    Teorema de Pitagoras

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ int main() {
     int a, b, c;
 
     for (a = 1; a <= 20; a++) {
-        for (b = a; b<= 20; b++)
+        for (b = a; b<= 20; b++) {
+            double c_double = sqrt (a*a + b*b);
+        }
     }
 }
