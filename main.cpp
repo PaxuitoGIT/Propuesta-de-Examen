16a5a6f16a67ab75343a5b5857ff8f0f121b3335
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-11-07 10:23:47 +0000

    Anotaciones para cada funcion

--- main.cpp
+++ main.cpp
@@ -2,17 +2,17 @@
 #include <cmath>
 
 int main() {
-    int a, b, c;
+    int a, b, c; //Nombramos a las variables
 
-    for (a = 1; a <= 20; a++) {
-        for (b = a; b <= 20; b++) {
-            double c_double = sqrt (a*a + b*b);
-            c = c_double;
+    for (a = 1; a <= 20; a++) { //Bucle para a que busque números del 1 al 20
+        for (b = a; b <= 20; b++) { //Bucle para b que busque números del 1 al 20
+            double c_double = sqrt (a*a + b*b); //Teorema de pitágoras para hallar el valor c
+            c = c_double; //Convertimos el valor c_double en un entero y que lo almacene en c
 
             
-            if (c_double == c) {
-                if (a + b > c && a + c > b && b + c > a) {
-                    printf ("%d - %d -%d\n", a,b,c);
+            if (c_double == c) { //Si c_double es igual a c, entonces se imprime el resultado
+                if (a + b > c && a + c > b && b + c > a) { //Verificamos si se cumple el enunciado del teorema de pitágoras
+                    printf ("%d - %d -%d\n", a,b,c); //Se imprime el resultado
                 }
             }
         }
